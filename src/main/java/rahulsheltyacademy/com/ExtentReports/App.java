f7e206bbb7735e39903b1af1bbe4fa34bedcf14a
Andrey <sugandrey1@gmail.com>
2021-03-29 17:56:04 -0400

    First commit

---
+++ src/main/java/rahulsheltyacademy/com/ExtentReports/App.java
@@ -0,0 +1,13 @@
+package rahulsheltyacademy.com.ExtentReports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/rahulsheltyacademy/com/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,39 @@
+package rahulsheltyacademy.com.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	//ExtentReports, ExtentSparkReporter - useful for creating of extent reports
+	@BeforeTest
+	public void config() {
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Andrew Sugrobov");
+	}
+	@Test
+	public void initialDemo() {
+		ExtentTest test = extent.createTest("Initial Demo");
+		System.setProperty("webdriver.chrome.driver", "C://Users/user/Downloads/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result doesn't match");
+		extent.flush();
+		
+	}
+
+}
